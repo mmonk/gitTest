71e1caaa5efda3d59a42b3f90c11cc11a7de94f9
matt monk <mmonk185@gmail.com>
2016-01-22 23:53:48 -0500

    2nd test

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("somecode git");
             Console.ReadLine();
+            Console.WriteLine("hswrj git");
         }
     }
 }
