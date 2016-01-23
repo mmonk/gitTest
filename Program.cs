b42e38f209f82834c1d0bc5ba9c4ed901d87b450
matt monk <mmonk185@gmail.com>
2016-01-22 23:55:15 -0500

    3rd commit

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("somecode git");
             Console.ReadLine();
-            Console.WriteLine("hswrj git");
+            Console.WriteLine("3rd git");
         }
     }
 }
